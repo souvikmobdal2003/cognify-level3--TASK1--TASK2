d95bcaf958ad8649c2d31caa9de002e93a7f4cfd
Souvik-Mondale <mondaksouvik289@gmail.com>
2024-08-18 02:20:04 +0530

    18-4-2024
    task2 completed

---
+++ task2/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navbarBurgers = Array.from(document.querySelectorAll('.navbar-burger'));
+
+    if (navbarBurgers.length > 0) {
+        navbarBurgers.forEach(el => {
+            el.addEventListener('click', () => {
+                const target = el.dataset.target;
+                const targetElement = document.getElementById(target);
+
+                el.classList.toggle('is-active');
+                targetElement.classList.toggle('is-active');
+            });
+        });
+    }
+});
